fd635cf87a2872f3dc21779ce75c784ea2a13fec
Mostak Khan <Mostak1993@gmail.com>
2025-04-14 18:31:37 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to react, this is a boilerplate</h1>
+      <p>This is just uses React frame with pure css.</p>
+    </div>
+  )
+}
+
+export default App
